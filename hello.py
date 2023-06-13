1e71fc2c870a03a948c6edf709a3a715180316ec
Jotinha124 <matiasjoaopedro19@gmail.com>
2023-06-13 12:42:56 +0100

    first commit

---
+++ hello.py
@@ -0,0 +1,4 @@
+def say_hi():
+    print('Hi, my name is João Pedro.')
+
+__version__ = '0.1'
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='hello',
+      version='1.0',
+      description='Hello Distribution Module',
+      author='João Pedro',
+      author_email='matiasjoaopedro19@gmail.com',
+      url='The URL project - github for example',
+      py_modules=['hello'],
+     )
